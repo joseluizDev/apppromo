908f84056deec658060313db7d524c708e3a8236
jose luizz <lui_zzzz@hotmail.com>
2024-12-04 16:34:57 -0300

    Adicionar biblioteca react-toastify e implementar notificações no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AdminDashboard } from './components/Admin/AdminDashboard';
 import { Login } from './components/Admin/AdminLogin';
 import { Home } from './components/Home/Home';
@@ -7,6 +9,18 @@ import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
