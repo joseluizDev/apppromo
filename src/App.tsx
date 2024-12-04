b9ebb38b97dff6b5ab8383b63088a72fe008b631
jose luizz <lui_zzzz@hotmail.com>
2024-12-04 14:54:36 -0300

    Remover componentes de banner e slider da página inicial e ajustar rotas para o painel administrativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AdminDashboard } from './components/Admin/AdminDashboard';
 import { AdminLogin } from './components/Admin/AdminLogin';
-import { Banner } from './components/Banner/Banner';
 import NotFound from './components/NotFound/NotFound';
-import { ProductSlider } from './components/ProductSlider/ProductSlider';
-import { PromoBanner } from './components/PromoBanner/PromoBanner';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/" element={
-          <div className="min-h-screen bg-gray-50">
-            <Banner />
-            <PromoBanner />
-            <main className="container mx-auto px-4 py-6">
-              <ProductSlider />
-            </main>
-          </div>
-        } />
+        
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
