a856bcc311109b102ae2fc98f6a7886f481644f7
jose luizz <lui_zzzz@hotmail.com>
2024-12-04 16:37:41 -0300

    Adicionar configuração de cache e otimizações no Vite, incluindo ajustes no Dockerfile e novos arquivos de dependência

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+    emptyOutDir: true,
+  },
+  cacheDir: '.vite_cache',
 });
