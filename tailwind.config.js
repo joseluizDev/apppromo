c090027761af5cdd3f3447db1f02b8e4f897524a
jose luizz <lui_zzzz@hotmail.com>
2024-12-04 14:45:31 -0300

    Adicionar configuração de dark mode e ajustar formatação no Tailwind e Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+
   theme: {
     extend: {},
   },
