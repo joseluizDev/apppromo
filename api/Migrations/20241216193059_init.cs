f4da24bfce82ea90579cebebe48f83c128fef92f
jose luizz <lui_zzzz@hotmail.com>
2024-12-16 16:32:24 -0300

    Feat: Adição do snapshot do modelo do banco de dados para a migração

---
+++ api/Migrations/20241216193059_init.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BackAppPromo.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Banner",
+                columns: table => new
+                {
+                    Ban_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Ban_titulo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ban_imagem = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Ban_temp = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banner", x => x.Ban_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    Cat_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Cat_nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.Cat_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Usu_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Usu_email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Usu_senha = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Usu_id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Pro_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Pro_titulo = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pro_descricao = table.Column<string>(type: "varchar(999)", maxLength: 999, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pro_preco = table.Column<decimal>(type: "decimal(15,2)", nullable: false),
+                    Pro_preco_promocional = table.Column<decimal>(type: "decimal(15,2)", nullable: false),
+                    Pro_quantidade = table.Column<int>(type: "int", nullable: false),
+                    Pro_data_cadastrado = table.Column<DateOnly>(type: "date", nullable: false),
+                    Cat_id = table.Column<int>(type: "int", nullable: false),
+                    Usu_id = table.Column<int>(type: "int", nullable: false),
+                    Pro_instagram = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pro_whats = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Pro_id);
+                    table.ForeignKey(
+                        name: "FK_Produto_Categoria_Cat_id",
+                        column: x => x.Cat_id,
+                        principalTable: "Categoria",
+                        principalColumn: "Cat_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Produto_Usuario_Usu_id",
+                        column: x => x.Usu_id,
+                        principalTable: "Usuario",
+                        principalColumn: "Usu_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Imagem",
+                columns: table => new
+                {
+                    Img_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Img_url = table.Column<string>(type: "varchar(999)", maxLength: 999, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pro_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Imagem", x => x.Img_id);
+                    table.ForeignKey(
+                        name: "FK_Imagem_Produto_Pro_id",
+                        column: x => x.Pro_id,
+                        principalTable: "Produto",
+                        principalColumn: "Pro_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Reserva",
+                columns: table => new
+                {
+                    Res_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Res_nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_email = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_telefone = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_instagram = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_endereco = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_referencia = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Res_entrega = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Res_retirada = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    Res_quantidade = table.Column<int>(type: "int", nullable: false),
+                    Res_valor_total = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Res_data_reserva = table.Column<DateOnly>(type: "date", nullable: false),
+                    Pro_id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reserva", x => x.Res_id);
+                    table.ForeignKey(
+                        name: "FK_Reserva_Produto_Pro_id",
+                        column: x => x.Pro_id,
+                        principalTable: "Produto",
+                        principalColumn: "Pro_id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Imagem_Pro_id",
+                table: "Imagem",
+                column: "Pro_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_Cat_id",
+                table: "Produto",
+                column: "Cat_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produto_Usu_id",
+                table: "Produto",
+                column: "Usu_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reserva_Pro_id",
+                table: "Reserva",
+                column: "Pro_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banner");
+
+            migrationBuilder.DropTable(
+                name: "Imagem");
+
+            migrationBuilder.DropTable(
+                name: "Reserva");
+
+            migrationBuilder.DropTable(
+                name: "Produto");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
